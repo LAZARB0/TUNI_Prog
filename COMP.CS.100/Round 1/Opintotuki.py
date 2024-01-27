a9cd331bdbb08ae59063c9a09a067a77ecd63047
Lassi Cederlöf <lassi.cederlof@tuni.fi>
2024-01-27 15:06:39 +0200

    Reset

--- COMP.CS.100/Round 1/Opintotuki.py
+++
@@ -1,7 +0,0 @@
-opintotuki = float(input("Enter the amount of the study benefits: "))
-uusi = (opintotuki * 1.0117)
-print("If the index raise is 1.17 percent, the study benefit,")
-print("after a raise, would be",uusi,"euros")
-kuusi = (uusi * 1.0117)
-print("and if there was another index raise, the study")
-print("benefits would be as much as",kuusi,"euros")
--- COMP.CS.100/Round 1/Tervehdys main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    nimi = input("Tell your name: ")
-    print("Hi", nimi)
-    print("your coding skills are great!")
-if __name__ == "__main__":
-    main()
